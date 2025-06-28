b428933b3ae057ae453a76b85694395e2d2dd98a
Akshay Asok <akshayasokson@gmail.com>
2025-06-28 20:53:01 +0530

    app completed

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,76 @@
 
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [hour, setHour] = useState(0);
+  const [time, setTime] = useState(0);
+
+  const [start, setStart] = useState(true);
+  const [reset, setReset] = useState(false);
+
+
+  const handleStart = () => {
+    setStart(false);
+  }
+
+  const handleStop = () => {
+    setStart(true);
+  }
+
+  const handleReset = () => {
+    setReset(true);
+  }
+
+  // useeffect
+  useEffect(() => {
+    
+    if(reset === true) {
+      setHour(0);
+      setTime(0);
+      setReset(false);
+    }
+
+    let timer;
+    if(start === false) {
+
+      timer = setTimeout(() => {
+
+        const temp = time + 1;
+
+        if (temp === 60) {
+          setHour(hour+1);
+          setTime(0);
+        }
+        else {
+          setTime(temp);
+        }
+
+      }, 1000)
+      
+
+    }
+
+    return () => {
+      clearTimeout(timer);
+    }
+
+  }, [hour, time, start, reset])
+
+
   return (
     <div className="App">
-      <h1>Stopwatch</h1>
       
+      <h1>Stopwatch</h1>
+
+      <p>{`Time: ${hour}:${time}`}</p>
+      
+      {
+        start ? (<button onClick={handleStart} >Start</button>) : (<button onClick={handleStop} >Stop</button>)
+      }
+      <button onClick={handleReset} >Reset</button>
+
     </div>
   );
 }
